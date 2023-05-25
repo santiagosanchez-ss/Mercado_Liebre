d0f5de1d7fd25f7db3182fc805c9b0926efd7130
Santiago Sanchez <s.sanchez2002@hotmail.com>
2023-05-24 21:31:55 -0300

    entrega

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const app = express ();
+const path =require ('path');
+
+app.use (express.static('public'));
+
+
+app.listen (process.env.PORT ||3000, ()=> {console.log('Servidor corriendo en le puerto 3000');})
+
+app.get ('/', (req, res)=> {
+    res.sendFile (path.join (__dirname, '/views/home.html'))} )
+
+app.get ('/register', (req, res)=> {
+    res.sendFile (path.join (__dirname, '/views/register.html'))} )
+
+app.get ('/login', (req, res)=> {
+    res.sendFile (path.join (__dirname, '/views/login.html'))} )        
